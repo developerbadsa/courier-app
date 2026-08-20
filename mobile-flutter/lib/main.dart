35901a4ee489fbb8bb858efebc22ac1617640816
Rahim_Badsa <rahimbadsa723@gmail.com>
2026-08-20 17:53:10 +0600

    feat(mobile): add runZonedGuarded global async crash shield and FlutterError handler in main.dart

--- mobile-flutter/lib/main.dart
+++ mobile-flutter/lib/main.dart
@@ -7,10 +7,29 @@ import 'features/auth/screens/splash_screen.dart';
 import 'features/rider/cubit/runsheet_cubit.dart';
 import 'features/rider/repositories/rider_repository.dart';
 
+import 'dart:async';
+import 'dart:developer' as developer;
+
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ShohnaatApp());
+  runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      
+      // Global Flutter UI Framework Error Handler
+      FlutterError.onError = (FlutterErrorDetails details) {
+        FlutterError.presentError(details);
+        developer.log('Flutter Error caught: ${details.exception}', stackTrace: details.stack);
+      };
+
+      runApp(const ShohnaatApp());
+    },
+    (Object error, StackTrace stack) {
+      // Global Asynchronous Error Shield
+      developer.log('Global Async Error caught: $error', stackTrace: stack);
+    },
+  );
 }
+
 
 class ShohnaatApp extends StatelessWidget {
   const ShohnaatApp({super.key});
